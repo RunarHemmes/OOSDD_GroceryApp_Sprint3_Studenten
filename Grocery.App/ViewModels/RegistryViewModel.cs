cdaf2cb320e457c5d1453a18e588484d1b76cd34
Runar <s1208862@student.windesheim.nl>
2025-09-26 14:37:41 +0200

    Finished UC9. A registration screen had been made, on wich a name, email, and password can be entered. The input is added to the clients as a new client, and can be used to log in.

---
+++ Grocery.App/ViewModels/RegistryViewModel.cs
@@ -0,0 +1,57 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.App.Views;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class RegistryViewModel : BaseViewModel
+    {
+        private readonly IAuthService _authService;
+        private readonly IClientService _clientService;
+        private readonly GlobalViewModel _global;
+
+        [ObservableProperty]
+        private string email = "";
+
+        [ObservableProperty]
+        private string password = "";
+
+        [ObservableProperty]
+        private string name = "";
+
+        [ObservableProperty]
+        private string loginMessage;
+
+        public RegistryViewModel(IAuthService authService, IClientService clientService, GlobalViewModel global)
+        { //_authService = App.Services.GetServices<IAuthService>().FirstOrDefault();
+            _authService = authService;
+            _clientService = clientService;
+            _global = global;
+        }
+
+        [RelayCommand]
+        private void ConfirmRegistry()
+        {
+            if (Email.Contains("@") != true || Email.EndsWith(".com") != true)
+            {
+                LoginMessage = "Voer een geldig emailadres in.\nEen geldig emailadres bevat een '@' en eindigt met '.com'.";
+            }
+            else
+            {
+                _clientService.AddClient(Name, Email, Password);
+                ToLogin();
+            }
+            return;
+        }
+
+        [RelayCommand]
+        public async Task ToLogin()
+        {
+            Routing.RegisterRoute(nameof(LoginView), typeof(LoginView));
+            await Shell.Current.GoToAsync(nameof(LoginView));
+        }
+    }
+}
